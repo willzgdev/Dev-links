062f51004473620c1dfae6c3ba9cace24b7702d0
willzgdev <williamzamignani@gmail.com>
2023-10-05 16:28:21 -0300

    li link corection

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function toggleMode() {
     //se tiver light mode, add a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    git
     //se tiver sem light mode, manter a imagem
     img.setAttribute("src", "./assets/avatar.png")
   }
